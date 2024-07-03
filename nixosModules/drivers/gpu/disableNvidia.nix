edc2f1cfb6359b84b932f75cd22a6ea5580f4a36
Veneficium <85629831+veneficium42@users.noreply.github.com>
2024-07-03 17:30:10 +0200

    update: add nvidiafb driver to blacklist

--- nixosModules/drivers/gpu/disableNvidia.nix
+++ nixosModules/drivers/gpu/disableNvidia.nix
@@ -19,6 +19,6 @@
             # Remove NVIDIA VGA/3D controller devices
             ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
         '';
-        boot.blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];
+        boot.blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_uvm" "nvidiafb" "nvidia_drm" "nvidia_modeset" ];
     };
 }
