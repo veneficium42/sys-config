f17ffd4889bcf762397c3aa628332302b0c8502b
Veneficium <85629831+veneficium42@users.noreply.github.com>
2024-12-01 23:17:49 +0100

    microvm: switch to cloud-hypervisor for better support

--- microvms/vaultwarden/configuration.nix
+++ microvms/vaultwarden/configuration.nix
@@ -1,13 +1,12 @@
 { config, ... }:
 {
   networking.hostName = "veneficium-microvm-vaultwarden";
-  microvm.hypervisor = "firecracker";
+  microvm.hypervisor = "cloud-hypervisor";
   microvm.shares = [
     {
       source = "/nix/store";
       mountPoint = "/nix/.ro-store";
       tag = "ro-store";
-      proto = "virtiofs";
     }
   ];
   system.stateVersion = config.system.nixos.version;
