2ba227f07820274b0ad8b462d4f765387537ac4b
Veneficium <85629831+veneficium42@users.noreply.github.com>
2024-12-01 22:53:31 +0100

    microvm: set stateVersion to host's

--- microvms/vaultwarden/configuration.nix
+++ microvms/vaultwarden/configuration.nix
@@ -1,5 +1,6 @@
-{ ... }:
+{ config, ... }:
 {
   networking.hostName = "veneficium-microvm-vaultwarden";
   microvm.hypervisor = "firecracker";
+  system.stateVersion = config.system.nixos.version;
 }
