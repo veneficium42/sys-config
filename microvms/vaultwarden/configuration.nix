94a35421614505d7d452f45b5d0cba58074028c1
Veneficium <85629831+veneficium42@users.noreply.github.com>
2024-12-01 23:12:24 +0100

    microvm: share nix store with host (significantly smaller builds)

--- microvms/vaultwarden/configuration.nix
+++ microvms/vaultwarden/configuration.nix
@@ -2,5 +2,13 @@
 {
   networking.hostName = "veneficium-microvm-vaultwarden";
   microvm.hypervisor = "firecracker";
+  microvm.shares = [
+    {
+      source = "/nix/store";
+      mountPoint = "/nix/.ro-store";
+      tag = "ro-store";
+      proto = "virtiofs";
+    }
+  ];
   system.stateVersion = config.system.nixos.version;
 }
