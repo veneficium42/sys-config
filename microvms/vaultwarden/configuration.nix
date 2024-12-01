2974d71ded51ef33de108a54f985dba582018d2c
Veneficium <85629831+veneficium42@users.noreply.github.com>
2024-12-01 23:21:39 +0100

    microvm: use virtiofs share

--- microvms/vaultwarden/configuration.nix
+++ microvms/vaultwarden/configuration.nix
@@ -7,6 +7,7 @@
       source = "/nix/store";
       mountPoint = "/nix/.ro-store";
       tag = "ro-store";
+      proto = "virtiofs";
     }
   ];
   system.stateVersion = config.system.nixos.version;
