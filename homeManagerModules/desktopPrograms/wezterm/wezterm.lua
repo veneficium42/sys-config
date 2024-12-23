553aca9f34ab07de3022ee9960bb947a951d3ec7
Veneficium <85629831+veneficium42@users.noreply.github.com>
2024-12-23 12:26:47 +0100

    wezterm: remove titlebar buttons

--- homeManagerModules/desktopPrograms/wezterm/wezterm.lua
+++ homeManagerModules/desktopPrograms/wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 return {
     front_end = "WebGpu";
     integrated_title_button_style = "Gnome";
-    window_decorations = "INTEGRATED_BUTTONS|RESIZE";
+    window_decorations = "TITLE | RESIZE";
     -- nix-replace
 }
