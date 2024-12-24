09df0a2ab86f4f18d63fab6346ef32c6f133d733
Veneficium <85629831+veneficium42@users.noreply.github.com>
2024-12-24 10:48:15 +0100

    fix outdated option

--- hosts/main-homelab/configuration.nix
+++ hosts/main-homelab/configuration.nix
@@ -43,7 +43,7 @@
   virtualisation.docker.enable = false;
   virtualisation.podman.enable = true;
   virtualisation.podman.dockerSocket.enable = true;
-  virtualisation.podman.defaultNetwork.dnsname.enable = true;
+  virtualisation.podman.defaultNetwork.settings.dns_enabled = true;
   virtualisation.arion = {
     backend = "podman-socket";
   };
