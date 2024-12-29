5b77289525f36007ed0f9040ed68fa7e28129bde
Veneficium <85629831+veneficium42@users.noreply.github.com>
2024-12-29 23:25:45 +0100

    format stuff

--- modules/home/desktopPrograms/wezterm/wezterm.lua
+++ modules/home/desktopPrograms/wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 return {
-    front_end = "WebGpu";
-    integrated_title_button_style = "Gnome";
-    window_decorations = "TITLE | RESIZE";
-    -- nix-replace
+	front_end = "WebGpu",
+	integrated_title_button_style = "Gnome",
+	window_decorations = "TITLE | RESIZE",
+	-- nix-replace
 }
