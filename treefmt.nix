039ece57cfa58e87aaff639159c62022ceaef190
Veneficium <85629831+veneficium42@users.noreply.github.com>
2024-12-29 23:24:06 +0100

    treefmt: enable various formatters

--- treefmt.nix
+++ treefmt.nix
@@ -3,4 +3,9 @@
   projectRootFile = "flake.nix";
 
   programs.nixfmt.enable = true;
+  programs.biome.enable = true;
+  programs.stylua.enable = true;
+  programs.taplo.enable = true;
+  programs.mdformat.enable = true;
+  programs.shellcheck.enable = true;
 }
