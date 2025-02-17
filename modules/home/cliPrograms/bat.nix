af8b0679d8596b783745bfdc99375f54a38b9ff8
Veneficium <85629831+veneficium42@users.noreply.github.com>
2025-02-17 22:56:18 +0100

    bat: fix typos

--- modules/home/cliPrograms/bat.nix
+++ modules/home/cliPrograms/bat.nix
@@ -1,10 +1,10 @@
 { config, lib, ... }:
 {
-  options.setting.programs.bat = {
+  options.settings.programs.bat = {
     enable = lib.mkEnableOption "enable bat, cat replacement";
   };
 
-  config = lib.mkIf config.settings.programs.bat {
+  config = lib.mkIf config.settings.programs.bat.enable {
     programs.bat.enable = true;
 
     programs.fish.shellAliases = lib.mkIf config.settings.shell.fish.enable {
