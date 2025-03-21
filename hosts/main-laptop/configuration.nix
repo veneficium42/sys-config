26afb28cb5a85d62dfedc13be9055991fc8687fa
Veneficium <85629831+veneficium42@users.noreply.github.com>
2025-03-21 17:24:27 +0100

    nix: setup nix-community cache

--- hosts/main-laptop/configuration.nix
+++ hosts/main-laptop/configuration.nix
@@ -19,6 +19,15 @@
 
   users.extraGroups.vboxusers.members = [ "fedfer" ];
 
+  nix.settings = {
+    substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   stylix.enable = true;
   stylix.autoEnable = true;
   stylix = {
